9a8a4da576e29b0013f0b31f959cfacaab48dee5
ryugyungyoon <alslehs@gmail.com>
2024-04-17 21:57:02 +0900

    :lipstick: design(user): 회원가입 페이지 화면 수정

--- src/main/java/com/jg/poms/controller/main/MainController.java
+++ src/main/java/com/jg/poms/controller/main/MainController.java
@@ -1,4 +1,4 @@
-package com.jg.poms.main.controller;
+package com.jg.poms.controller.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
