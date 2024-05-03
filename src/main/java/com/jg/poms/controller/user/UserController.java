3acaba16c0aebd077d3e23b330724c2454294e2c
Park Jisoo <coya319@naver.com>
2024-05-03 22:29:31 +0900

    :sparkles: feat(login): 로그인 기능 추가(임시 커밋)

--- src/main/java/com/jg/poms/controller/user/UserController.java
+++ src/main/java/com/jg/poms/controller/user/UserController.java
@@ -2,6 +2,7 @@ package com.jg.poms.controller.user;
 
 import com.jg.poms.domain.user.User;
 import com.jg.poms.dto.user.request.UserCreateRequest;
+import com.jg.poms.dto.user.request.UserLoginRequest;
 import com.jg.poms.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Slf4j
+@RequestMapping("user")
 public class UserController {
 
     private final UserService userService;
@@ -17,27 +19,57 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/login-form")
+    @GetMapping("login-form")
     public String loginForm(){
-        return "user/login-form";
+        return "nonlogin/login-form";
     }
 
-    @GetMapping("/terms-form")
+    @PostMapping("login")
+    @ResponseBody
+    public String User(UserLoginRequest request){
+        //return userService.saveUser(request);
+        System.out.println("로그인 아이디: "+ request.getId());
+        return request.getId();
+    }
+    /**
+     * [로그인 성공후 이동]
+     *
+     * @author zisooya
+     * @return String
+     */
+    @GetMapping("login-success")
+    public String loginSuccess(){
+        return "redirect:/main";
+    }
+
+    /**
+     * [로그인 실패후 이동]
+     *
+     * @author zisooya
+     * @return String
+     */
+    @GetMapping("login-fail")
+    public String loginFail(){
+        return "nonlogin/login_form";
+    }
+
+
+    @GetMapping("terms-form")
     public String termsForm(){
         return "user/terms";
     }
 
-    @GetMapping("/join-form")
+    @GetMapping("join-form")
     public String joinForm(){
         return "user/join";
     }
 
-    @PostMapping("/save-user")
+    @PostMapping("save-user")
     @ResponseBody
     public User saveUser(UserCreateRequest request){
         return userService.saveUser(request);
     }
-    @PostMapping("/certify")
+    @PostMapping("certify")
     @ResponseBody
     public String certify(@RequestParam("contactNumber") String contactNumber){
         log.debug(contactNumber);
---
+++ src/main/java/com/jg/poms/dto/user/request/UserLoginRequest.java
@@ -0,0 +1,11 @@
+package com.jg.poms.dto.user.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserLoginRequest {
+    private String id;
+    private String pw;
+}
