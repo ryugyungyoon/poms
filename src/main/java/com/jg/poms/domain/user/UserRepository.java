60b900040b1f52e3ab989ca4f6f64296d59161fa
Park Jisoo <coya319@naver.com>
2024-05-07 15:37:17 +0900

    임시 커밋 - 로그인 기능

--- src/main/java/com/jg/poms/domain/user/UserRepository.java
+++ src/main/java/com/jg/poms/domain/user/UserRepository.java
@@ -2,5 +2,10 @@ package com.jg.poms.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+	boolean existsById(String id);
+	//boolean existsByNickname(String nickname);
+	Optional<User> findById(String id);
 }
