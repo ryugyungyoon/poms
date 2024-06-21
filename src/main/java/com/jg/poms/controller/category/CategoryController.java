15d9c45b327afc7c1a7b44d0f7aa1f4919bd240d
zisooya <zisooyq319@gmail.com>
2024-06-21 19:16:21 +0900

    :recycle: refactor(category): 카테고리 리스트 조회 기능 리팩토링 - url 매핑으로 공통 로직 타도록 수정 - url 매핑시 하위 카테고리 조회하여 model 객체 넘기도록 수정

--- src/main/java/com/jg/poms/controller/category/CategoryController.java
+++ src/main/java/com/jg/poms/controller/category/CategoryController.java
@@ -2,11 +2,14 @@ package com.jg.poms.controller.category;
 
 import com.jg.poms.core.http.HttpBuilder;
 import com.jg.poms.core.http.ResponseVO;
+import com.jg.poms.dto.category.response.CategoryResponse;
 import com.jg.poms.service.category.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/category")
 @Controller
@@ -22,12 +25,20 @@ public class CategoryController{
 	 *
 	 * @author zisooya
 	 */
-	@GetMapping("women/list-form")
-	public String listForm(Model model) {
-		//검색 조건
-		//model.addAttribute("searchForm", searchForm);
+	@GetMapping("{categoryIdx}/list-form")
+	public String listForm(Model model, @PathVariable(value="categoryIdx") Long categoryIdx) {
+		List<CategoryResponse> subCategoryList;
+		String pageUrl = "";
 
-		return "/menu/category/women/category_women_list";
+		//카테고리 별 페이지 반환
+		if(categoryIdx == 4){
+			subCategoryList = categoryService.getSubCategoryList(categoryIdx);
+			model.addAttribute("subCategoryList", subCategoryList);
+			pageUrl = "/menu/category/women/category_women_list";
+		}
+
+		//model.addAttribute("searchForm", searchForm);
+		return pageUrl;
 	}
 	/**
 	 * [카테고리 전체 리스트 조회]
