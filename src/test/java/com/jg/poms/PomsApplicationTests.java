b39ef0d15169a980389d630dfd5eaf3c47e57811
zisooya <coya319@naver.com>
2024-08-08 19:56:42 +0900

    :white_check_mark: test(profile): profile 체크 테스트 추가

--- src/test/java/com/jg/poms/PomsApplicationTests.java
+++ src/test/java/com/jg/poms/PomsApplicationTests.java
@@ -1,13 +1,27 @@
 package com.jg.poms;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PomsApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void ProfileCheck () {
+		//when
+		String profile = this.restTemplate.getForObject("/profile", String.class);
+
+		//then
+		assertThat(profile).isEqualTo("local");
+	}
 }
